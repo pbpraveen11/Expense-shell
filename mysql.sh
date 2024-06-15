4d6f4c6030152d94c6062843191944eb65d7f0ec
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-15 16:28:38 +0530

    Colors Script

---
+++ mysql.sh
@@ -0,0 +1,22 @@
+$!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi    
+}
+
