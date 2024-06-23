a5f4e0bd965ba8660b60dbf9dacfacb8dfd95287
pbpraveen11 <pbpraveenkumar11@gmail.com>
2024-06-23 18:55:38 +0530

    Frontend services

---
+++ frontend.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi    
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root acces."
+    exit 1 # manually exit if error comes
+else
+    echo "You are a super user"    
+fi
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "Removing existing files"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+VALIDATE $? "Extracting frontend code"
+
+#check your repo and path
+cp /home/ec2-user/Expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Cpoied expense conf"
+
+systemctl restart nginx
+VALIDATE $? "Restarting nginx"
